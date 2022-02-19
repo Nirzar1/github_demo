e2a75b0596cadb96030a8938010045a38e2c739f
nirzarshah <nirzar_shah@persistent.com>
2022-02-19 17:23:41 +0530

    Added function headers

--- math.py
+++ math.py
@@ -1,18 +1,12 @@
 # add implementation
 def add(x,y):
-    return x+y  # onBug123
-    
+    pass
 # subtract implementation
 def subtract(x,y):
-    return x-y  # on Master 
-    
+    pass
 # multiply implementation
 def multiply(x,y):
-    return x*y  # on Bug456
-    
+    pass
 # divide implementation
 def divide(x,y):
-    if y == 0 :     #on Bug789
-        return DIVIDE BY_0_ERROR
-    else :
-        return x/y  
+    pass
